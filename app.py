01dc698d8b4ba2a879f9312a07ade97247cab6bf
martin.chodur <m.chodur@seznam.cz>
2016-04-01 22:59:34 +0200

    ...

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-import flask
+from flask import Flask
 
+app = Flask(__name__)
 
-def wsgi_app(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    response_body = str(dir(flask))
-    yield response_body.encode()
 
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-
-    httpd = make_server('localhost', 5555, wsgi_app)
-    httpd.serve_forever()
+    app.run()
