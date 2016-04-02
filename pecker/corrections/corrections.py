519684e6e15b44ee00d9811fcb3579b79c17d086
martin.chodur <m.chodur@seznam.cz>
2016-04-02 15:33:08 +0200

    ...

--- pecker/corrections/corrections.py
+++ pecker/corrections/corrections.py
@@ -4,7 +4,7 @@ from flask import request
 from pecker.geotools.point import Point
 from pecker.geotools.point_buffer import PointBuffer
 from pecker.model.correction_db_handler import CorrectionDbHandler
-from pecker.geotools.goujeson_builder import GeojsonBuilder
+from pecker.geotools.geojson_builder import GeojsonBuilder
 from pecker.app import app
 
 
