867ea28bbb18a8e78746567a6f5ab8fb85c5124c
Ivo Pisařovic <xpisarov@node.mendelu.cz>
2016-04-02 15:33:26 +0200

    no message

--- pecker/user/authentication.py
+++ pecker/user/authentication.py
@@ -12,30 +12,32 @@ def login():
     # init cookies
     C = Cookie.SimpleCookie()
 
-    # Create your consumer with the proper key/secret.
-    consumer = oauth.Consumer(key="QH9G5kSSPpFCsZ0Brs9p3Ntvw", 
-        secret="qDcFA4afHRo3VJvvANQ3BTITHg2GdQYTGzATpth5caYmmTk9dO")
-
-    # Request token URL for Twitter.
-    request_token_url = "https://api.twitter.com/oauth/request_token"
-
-    # Create our client.
-    client = oauth.Client(consumer)
-
-    # The OAuth Client request works just like httplib2 for the most part.
-    resp, content = client.request(request_token_url, "GET")
-    
-    results = urlparse.parse_qsl(content)
-
-    if results[1][0]=='oauth_verifier':
+    if request.args.get('oauth_verifier')!='':
         uid = C["session_id"]
         C = Cookie.SimpleCookie()
         puvodni = SessionDbHandler.get_session(uid, "oauth_token")
-        if results[0][1]==puvodni:
+        if request.args.get('oauth_token')==puvodni:
             return "ok"
         else: 
             return "Cannot log in."
+
     else:
+
+        # Create your consumer with the proper key/secret.
+        consumer = oauth.Consumer(key="QH9G5kSSPpFCsZ0Brs9p3Ntvw", 
+            secret="qDcFA4afHRo3VJvvANQ3BTITHg2GdQYTGzATpth5caYmmTk9dO")
+
+        # Request token URL for Twitter.
+        request_token_url = "https://api.twitter.com/oauth/request_token"
+
+        # Create our client.
+        client = oauth.Client(consumer)
+
+        # The OAuth Client request works just like httplib2 for the most part.
+        resp, content = client.request(request_token_url, "GET")
+
+        results = urlparse.parse_qsl(content)
+
         uid = str(uuid.uuid4())
         C["session_id"] = uid
         SessionDbHandler.create_session(uid, "oauth_token", results[0][1])
