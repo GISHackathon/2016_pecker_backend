725065b2ad4615afa4f6aa213b98f2c23156d663
martin.chodur <m.chodur@seznam.cz>
2016-04-02 05:31:40 +0200

    ...

--- pecker/app.py
+++ pecker/app.py
@@ -1,13 +1,11 @@
 from flask import Flask
 
-
+app = Flask(__name__)
 
 from pecker.user.authentication import *
 from pecker.errors.error_importer import *
 from pecker.errors.errors import *
 
-
-app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=80, debug=True)
