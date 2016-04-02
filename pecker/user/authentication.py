e540a86884cc6fda62d11f34c86cbef21af04210
Ivo Pisařovic <xpisarov@node.mendelu.cz>
2016-04-02 12:16:37 +0200

    ,,,

--- pecker/user/authentication.py
+++ pecker/user/authentication.py
@@ -18,8 +18,6 @@ def login():
 
     # The OAuth Client request works just like httplib2 for the most part.
     resp, content = client.request(request_token_url, "GET")
-    return resp + content
+    return "heello"
 
-    #r = requests.post('https://api.twitter.com/oauth/request_token', auth='OAuth oauth_callback="http%3A%2F%2Flocalhost%2Fsign-in-with-twitter%2F",               oauth_consumer_key="QH9G5kSSPpFCsZ0Brs9p3Ntvw",              oauth_nonce="ea9ec8429b68d6b77cd5600adbbb0456",              oauth_signature="F1Li3tvehgcraF8DMJ7OyxO4w9Y%3D",              oauth_signature_method="HMAC-SHA1",              oauth_timestamp="1318467427",              oauth_version="1.0"')
-
-    #return r.text
+   
