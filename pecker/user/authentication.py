92db384882a210108e2d6774f748ce6f0627c29c
Ivo Pisařovic <xpisarov@node.mendelu.cz>
2016-04-02 12:22:57 +0200

    ,,,

--- pecker/user/authentication.py
+++ pecker/user/authentication.py
@@ -19,8 +19,8 @@ def login():
     # The OAuth Client request works just like httplib2 for the most part.
     resp, content = client.request(request_token_url, "GET")
 
-    parts = parse_qs(content)
+    #parts = parse_qs(content)
 
-    return parts
+    return content
 
    
