ccbf907b1f0ef90a0b80577c9c793e9941409c9c
Ivo Pisařovic <xpisarov@node.mendelu.cz>
2016-04-02 11:53:59 +0200

    ,,,

--- pecker/user/authentication.py
+++ pecker/user/authentication.py
@@ -1,6 +1,9 @@
 from pecker.app import app
-
+from requests import requests
 
 @app.route('/login')
 def login():
-    return 'login'
+
+    r = requests.post('https://api.twitter.com/oauth/request_token')
+
+    return r.text
