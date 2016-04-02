93a7f875436e272b09b6dc0ce56d28c144687032
martin.chodur <m.chodur@seznam.cz>
2016-04-02 15:44:30 +0200

    ...

--- pecker/errors/error_importer.py
+++ pecker/errors/error_importer.py
@@ -13,12 +13,12 @@ def import_errors():
         resp = urllib2.urlopen(config.RU_WFS_URL)
     except urllib2.URLError as e:
         print 'err geoserver request...'
-        return
+        return ''
     data_json = json.load(resp)
     for f in data_json['features'][:config.IMP_ERR_COUNT]:
         coord = f['geometry']['coordinates']
         kat_id = f['properties']['kod']
         ErrorDbHandler.import_error(kat_id, coord[0], coord[1])
-    return
+    return ''
 
 
